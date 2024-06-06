d97dec91b44249598e8d589eea1aa506789e7df0
AdamFatherOfCreator <140255333+AdamFatherOfCreator@users.noreply.github.com>
2024-06-06 18:31:38 +0700

    Update whitelist.lua
    Signed-off-by: AdamFatherOfCreator <140255333+AdamFatherOfCreator@users.noreply.github.com>

--- whitelist.lua
+++ whitelist.lua
@@ -12,6 +12,6 @@ return {
 "60502cb4-9fe8-4b24-bb88-83e299e82e3d", -- Duy Pham
 "4e626557-4b70-4665-9d23-1f5626a1ca51", -- Trần Minh Đạt
 "903fab9d-d05a-4667-8b81-eae73cf4a7d4", -- Huyy Niee
-"c37e0ba8-a009-42d6-a1a4-333665d15f9d", -- Huyy Niee 2
+"5d3460fc-c76c-45d6-bba5-d96e8c3c712f", -- Huyy Niee 2
 "b23a2767-bb43-4ebd-beed-a4868f33f411" -- Dương ngu 
 }
