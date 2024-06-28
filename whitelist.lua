7d56bd7b2cff27d9ead1ba2eb67b906af8745c5b
AdamFatherOfCreator <140255333+AdamFatherOfCreator@users.noreply.github.com>
2024-06-28 12:49:58 +0700

    Update whitelist.lua
    Signed-off-by: AdamFatherOfCreator <140255333+AdamFatherOfCreator@users.noreply.github.com>

--- whitelist.lua
+++ whitelist.lua
@@ -2,7 +2,7 @@ return {
 "c4935109-282c-447b-99cb-b24ea3946e4e", -- SALUNA
 "5e582884-7bf4-49bf-852f-e3ccda5d184f", -- Khôi ngu
 "9891adb9-a4a2-4907-be40-7db9ddae36c3", -- Hiếu Trung
-"f52bd739-eb67-4419-826e-e83fcd806ef6", -- Khoa GK
+"b7fed3be-5c11-491a-b56b-8ecb5006a08a", -- Khoa GK
 "1eac697e-e753-45cf-bb54-f491e8ba1007", -- Huy Tú
 "cc7482e2-9357-40f7-a9b7-237a580f4054", -- Huy Tú 2
 "a4cb5880-52e9-4da6-abc1-d512a580b0f1", -- Nguyễn Anh Tiến
